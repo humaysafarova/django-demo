7f777a0fb8ade96a19565325a5329017ca7fc466
humaysafarova <166644215+humaysafarova@users.noreply.github.com>
2025-04-22 20:33:20 +0000

    changed

--- core/settings.py
+++ core/settings.py
@@ -31,9 +31,11 @@ INSTALLED_APPS = [
     # "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -41,6 +43,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://humaysafarova.github.io",
 ]
 
 ROOT_URLCONF = "core.urls"
